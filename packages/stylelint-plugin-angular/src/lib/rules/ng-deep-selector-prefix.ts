e6b504e9f8b0d47ea100454239bb88e363840d76
strawberry-choco <kangrui.ye@cloudflight.io>
2022-09-30 12:12:07 +0200

    update stylelint version (#6)
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>
    
    Signed-off-by: Kangrui Ye <kangrui.ye@cloudflight.io>

--- packages/stylelint-plugin-angular/src/lib/rules/ng-deep-selector-prefix.ts
+++ packages/stylelint-plugin-angular/src/lib/rules/ng-deep-selector-prefix.ts
@@ -1,31 +1,35 @@
 import * as stylelint from 'stylelint';
+import { Rule } from 'stylelint';
 
 const ruleName = '@cloudflight-stylelint-angular/ng-deep-selector-prefix';
 
-export const ngDeepSelectorPrefix = stylelint.createPlugin(
-  ruleName,
-  () => (postcssRoot, postcssResult) => {
-    const validOptions = stylelint.utils.validateOptions(postcssResult, ruleName);
+const fn: Rule = () => (postcssRoot, postcssResult) => {
+  const validOptions = stylelint.utils.validateOptions(postcssResult, ruleName);
 
-    if (!validOptions) {
-      return;
-    }
-
-    for (const node of postcssRoot.nodes) {
-      if (node.type !== 'rule' || !node.selector.trimStart().startsWith('::ng-deep')) {
-        continue;
-      }
-
-      stylelint.utils.report({
-        ruleName,
-        message: 'Unexpected ng-deep selector without prefix',
-        result: postcssResult,
-        node,
-        line: node.source?.start?.line,
-        word: node.selector,
-      });
-    }
+  if (!validOptions) {
+    return;
   }
-);
 
-ngDeepSelectorPrefix.ruleName = ruleName;
+  for (const node of postcssRoot.nodes) {
+    if (
+      node.type !== 'rule' ||
+      !node.selector.trimStart().startsWith('::ng-deep')
+    ) {
+      continue;
+    }
+
+    stylelint.utils.report({
+      ruleName,
+      message: 'Unexpected ng-deep selector without prefix',
+      result: postcssResult,
+      node,
+      line: node.source?.start?.line,
+      word: node.selector,
+    });
+  }
+};
+
+fn.ruleName = ruleName;
+fn.messages = {};
+
+export const ngDeepSelectorPrefix = stylelint.createPlugin(ruleName, fn);
