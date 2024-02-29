8409d142aadde754b77409e34cca6f38461459a6
Kangrui Johann Ye <kangrui.ye@cloudflight.io>
2024-02-29 11:42:10 +0100

    ignore deep pseudo class for vuejs
    Signed-off-by: Kangrui Johann Ye <kangrui.ye@cloudflight.io>

--- packages/stylelint-plugin-vue/src/index.js
+++ packages/stylelint-plugin-vue/src/index.js
@@ -1,4 +1,17 @@
 /** @type {import('stylelint').Config} */
 module.exports = {
     extends: ['@cloudflight/stylelint-plugin', 'stylelint-config-html/vue'],
+    overrides: [
+        {
+            files: ['*.vue'],
+            rules: {
+                'selector-pseudo-class-no-unknown': [
+                    true,
+                    {
+                        ignorePseudoClasses: ['deep'],
+                    },
+                ],
+            },
+        },
+    ],
 };
